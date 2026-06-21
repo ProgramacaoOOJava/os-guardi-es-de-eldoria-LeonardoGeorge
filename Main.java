5f1c2c61661449c2e8ec0064a4b79235a8393ec0
Leonardo George <leonardogeorge30@gmail.com>
2026-06-21 01:50:00 +0000

    Criado Personagem...     - Classe     - Atributos     - Construtor     - Exiição de Status Criado Main...     - 3 novos Personagens     - Chamada da exibição de Status de cada Personagem

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
-
-        .
-        .
-        .
+        Personagem heroi2 = new Personagem("Lotus", "Arqueira", 1, 85, 13.5);
+        Personagem heroi3 = new Personagem("Alquimista", "Mago", 1, 65, 16);
+        
+        
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
+        heroi3.exibirStatus();
 
     }
 }
